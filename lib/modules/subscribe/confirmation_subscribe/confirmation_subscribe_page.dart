f5495838774b3b9deb8710001d3e684ceff85874
amXCVI <am.xcvi@gmail.com>
2022-08-22 18:09:06 +0300

    add confirm subscribe checker

--- lib/modules/subscribe/confirmation_subscribe/confirmation_subscribe_page.dart
+++ lib/modules/subscribe/confirmation_subscribe/confirmation_subscribe_page.dart
@@ -2,25 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medlike/domain/app/cubit/subscribe/subscribe_cubit.dart';
 import 'package:medlike/domain/app/cubit/user/user_cubit.dart';
+import 'package:medlike/modules/subscribe/confirmation_subscribe/agreements_checker.dart';
 import 'package:medlike/modules/subscribe/confirmation_subscribe/appointment_info.dart';
 import 'package:medlike/modules/subscribe/confirmation_subscribe/confirmation_action_button.dart';
 import 'package:medlike/modules/subscribe/confirmation_subscribe/payment_widget.dart';
 import 'package:medlike/modules/subscribe/confirmation_subscribe/user_info.dart';
+import 'package:medlike/themes/colors.dart';
 import 'package:medlike/widgets/default_scaffold/default_scaffold.dart';
 import 'package:medlike/widgets/dividers/dash_divider.dart';
 import 'package:medlike/widgets/scrollbar/default_scrollbar.dart';
 
-class ConfirmationSubscribePage extends StatelessWidget {
+class ConfirmationSubscribePage extends StatefulWidget {
   const ConfirmationSubscribePage({Key? key, required this.userId})
       : super(key: key);
 
   final String userId;
 
   @override
+  State<ConfirmationSubscribePage> createState() =>
+      _ConfirmationSubscribePageState();
+}
+
+class _ConfirmationSubscribePageState extends State<ConfirmationSubscribePage> {
+  late bool isCheckedAgreements = false;
+
+  void setIsCheckedValue(bool value) {
+    setState(() {
+      isCheckedAgreements = value;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    void _createNewAppointment() {
+      if (!isCheckedAgreements) {
+        return;
+      }
+      context.read<SubscribeCubit>().createNewAppointment(
+            userId: widget.userId,
+            userName: context.read<UserCubit>().getShortUserName(widget.userId),
+          );
+    }
+
     return WillPopScope(
       onWillPop: () async {
-        context.read<SubscribeCubit>().unlockCell(userId: userId);
+        context.read<SubscribeCubit>().unlockCell(userId: widget.userId);
         Navigator.pop(context);
         return false;
       },
@@ -28,30 +54,33 @@ class ConfirmationSubscribePage extends StatelessWidget {
         appBarTitle: 'Запись на прием',
         isChildrenPage: true,
         actionButton: FloatingActionButton.extended(
-            onPressed: () {
-              context.read<SubscribeCubit>().createNewAppointment(
-                    userId: userId,
-                    userName:
-                        context.read<UserCubit>().getShortUserName(userId),
-                  );
-            },
+            onPressed: _createNewAppointment,
+            backgroundColor: isCheckedAgreements
+                ? Theme.of(context).primaryColor
+                : AppColors.lightText,
             extendedPadding: const EdgeInsets.all(15),
             label: const ConfirmationActionButtonLabel()),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: DefaultScrollbar(
             child: ListView(
               shrinkWrap: true,
               children: [
-                UserInfo(userId: userId),
+                const SizedBox(height: 20),
+                UserInfo(userId: widget.userId),
                 const SizedBox(height: 19),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: DashDivider(),
-                ),
+                const DashDivider(),
                 const SizedBox(height: 24),
                 const PaymentWidget(),
                 const AppointmentInfo(),
+                const SizedBox(height: 24),
+                const DashDivider(),
+                const SizedBox(height: 24),
+                AgreementsChecker(
+                  isChecked: isCheckedAgreements,
+                  setIsCheckedValue: setIsCheckedValue,
+                ),
+                const SizedBox(height: 44),
               ],
             ),
           ),
