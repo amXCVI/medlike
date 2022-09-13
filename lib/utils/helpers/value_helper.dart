2c66416b2a8065a56e7ffdc9a05eedbd6dc7c418
Василий Смоляр <smolyar4@gmail.com>
2022-09-13 16:42:42 +0300

    Fix: dates

--- lib/utils/helpers/value_helper.dart
+++ lib/utils/helpers/value_helper.dart
@@ -110,14 +110,17 @@ class ValueHelper {
   }
 
   static getDayString(DateTime date) {
-    if(date.day == DateTime.now().day) {
-      return 'Сегодня';
-    } else if(date.day == DateTime.now().day - 1) {
-      return 'Вчера';
-    } else {
-      DateFormat dateFormat = DateFormat("dd MMMM", 'ru_RU');
-      return dateFormat.format(date);
+    final now = DateTime.now();
+
+    if(date.month == now.month && date.year == date.year) {
+      if(date.day == now.day) {
+        return 'Сегодня';
+      } else if(date.day == now.day - 1) {
+        return 'Вчера';
+      } 
     }
+    DateFormat dateFormat = DateFormat("dd MMMM", 'ru_RU');
+    return dateFormat.format(date);
   }
 
   /// Возвращает текстовое представления периода
