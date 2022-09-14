219c4fcc216445388a21fc7ef76edea93cbf63b9
amXCVI <am.xcvi@gmail.com>
2022-09-14 15:22:58 +0300

    fix padding for action button on support page

--- lib/modules/login/unauth_support/unauth_support_page.dart
+++ lib/modules/login/unauth_support/unauth_support_page.dart
@@ -54,7 +54,7 @@ class UnauthSupportPage extends StatelessWidget {
           );
         },
       ),
-      bottomNavigationBar: const SizedBox(),
+      bottomNavigationBar: const SizedBox(height: 56),
     );
   }
 }
