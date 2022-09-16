51338a6adacea9e6725569f542aa3c5afbd5ccaf
Василий Смоляр <smolyar4@gmail.com>
2022-09-16 13:53:03 +0300

    Fix: chips

--- lib/modules/health/diary_page/diary_chips.dart
+++ lib/modules/health/diary_page/diary_chips.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medlike/constants/app_constants.dart';
 import 'package:medlike/domain/app/cubit/diary/diary_cubit.dart';
 import 'package:medlike/themes/colors.dart';
 
@@ -25,21 +26,36 @@ class DiaryChips extends StatelessWidget {
         }
         const width = 10.0;
 
+        final list = [
+          DiaryChip(grouping: 'Hour', title: 'Час', onTap: tap, selected: selectedGroup),
+          const SizedBox(width: width),
+          DiaryChip(grouping: 'Day', title: 'День', onTap: tap, selected: selectedGroup),
+          const SizedBox(width: width),
+          DiaryChip(grouping: 'Week', title: 'Неделя', onTap: tap, selected: selectedGroup),
+          const SizedBox(width: width),
+          DiaryChip(grouping: 'Month', title: 'Месяц', onTap: tap, selected: selectedGroup),
+        ];
+
+        if(MediaQuery.of(context).size.width <= AppConstants.smScreenWidth) {
+          return Container(
+            width: MediaQuery.of(context).size.width,
+            height: 64,
+            padding: const EdgeInsets.only(top: 10, bottom: 10),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: list
+            )
+          );
+        }
+
         return Container(
           width: MediaQuery.of(context).size.width,
           height: 64,
           padding: const EdgeInsets.only(top: 10, bottom: 10),
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              DiaryChip(grouping: 'Hour', title: 'Час', onTap: tap, selected: selectedGroup),
-              const SizedBox(width: width),
-              DiaryChip(grouping: 'Day', title: 'День', onTap: tap, selected: selectedGroup),
-              const SizedBox(width: width),
-              DiaryChip(grouping: 'Week', title: 'Неделя', onTap: tap, selected: selectedGroup),
-              const SizedBox(width: width),
-              DiaryChip(grouping: 'Month', title: 'Месяц', onTap: tap, selected: selectedGroup),
-            ],
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.min,
+            children: list,
           ),
         );
       },
@@ -63,30 +79,36 @@ class DiaryChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: InkWell(
-        onTap: () => onTap(grouping),
-        child: Chip(
-          backgroundColor: selected == grouping ? AppColors.mainBrandColor : AppColors.mainAppBackground,
-          shadowColor: const Color.fromRGBO(0, 0, 0, 0.5),
-          elevation: 8,
-          label: Flex(
-            direction: Axis.horizontal,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                title,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: selected == grouping ? AppColors.mainAppBackground : AppColors.mainText,
-                  fontWeight: FontWeight.w400
-                ),
-                overflow: TextOverflow.ellipsis,
-              )
-            ],
-          ),
+    final widget = InkWell(
+      onTap: () => onTap(grouping),
+      child: Chip(
+        backgroundColor: selected == grouping ? AppColors.mainBrandColor : AppColors.mainAppBackground,
+        shadowColor: const Color.fromRGBO(0, 0, 0, 0.5),
+        elevation: 8,
+        label: Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: selected == grouping ? AppColors.mainAppBackground : AppColors.mainText,
+                fontWeight: FontWeight.w400
+              ),
+              overflow: TextOverflow.ellipsis,
+            )
+          ],
         ),
       ),
     );
+
+    if(MediaQuery.of(context).size.width > AppConstants.smScreenWidth) {
+      return Expanded(
+        child: widget
+      );
+    }
+
+    return widget;
   }
 }
