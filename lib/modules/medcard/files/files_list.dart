cc97ab43c1801363fe36f46dfb6e777a9d7fe586
amXCVI <am.xcvi@gmail.com>
2022-09-20 11:35:37 +0300

    fix merge conflicts

--- lib/modules/medcard/files/files_list.dart
+++ lib/modules/medcard/files/files_list.dart
@@ -6,7 +6,6 @@ import 'package:flutter_svg/svg.dart';
 import 'package:medlike/data/models/medcard_models/medcard_models.dart';
 import 'package:medlike/domain/app/cubit/medcard/medcard_cubit.dart';
 import 'package:medlike/modules/medcard/files/file_item.dart';
-import 'package:medlike/modules/medcard/files/not_found_user_files.dart';
 import 'package:medlike/utils/api/api_constants.dart';
 import 'package:medlike/widgets/not_found_data/empty_list_widget.dart';
 import 'package:medlike/widgets/scrollbar/default_scrollbar.dart';
@@ -46,44 +45,49 @@ class FilesList extends StatelessWidget {
 
     return RefreshIndicator(
         onRefresh: () => onRefreshData(isRefresh: true),
-        child: userFilesList.isNotEmpty ? DefaultScrollbar(
-          child: ListView(
-            controller: listController,
-            shrinkWrap: true,
-            children: [
-              ...userFilesList
-                  .map((item) => Slidable(
-                        key: UniqueKey(),
-                        endActionPane: ActionPane(
-                          motion: const BehindMotion(),
-                          dismissible: DismissiblePane(
-                            onDismissed: () {
-                              handleDeleteFile(item.id);
-                            },
-                          ),
-                          children: [
-                            Expanded(
-                              child: InkWell(
-                                onTap: () {
-                                  handleDeleteFile(item.id);
-                                },
-                                child: Container(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 34.0),
-                                  decoration: BoxDecoration(
-                                    color: Theme.of(context).errorColor,
-                                  ),
-                                  child: Expanded(
-                                    child: Center(
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.end,
-                                        children: [
-                                          const SizedBox(width: 20.0),
-                                          SvgPicture.asset(
-                                              'assets/icons/appointments/ic_delete_appointment.svg'),
-                                          const SizedBox(width: 20.0),
-                                        ],
+        child: userFilesList.isEmpty
+            ? const EmptyListWidget(
+                imgPath: 'assets/images/empty_files.png',
+                label: 'Здесь будет список ваших файлов')
+            : DefaultScrollbar(
+                child: ListView(
+                  controller: listController,
+                  shrinkWrap: true,
+                  children: [
+                    ...userFilesList
+                        .map((item) => Slidable(
+                              key: UniqueKey(),
+                              endActionPane: ActionPane(
+                                motion: const BehindMotion(),
+                                dismissible: DismissiblePane(
+                                  onDismissed: () {
+                                    handleDeleteFile(item.id);
+                                  },
+                                ),
+                                children: [
+                                  Expanded(
+                                    child: InkWell(
+                                      onTap: () {
+                                        handleDeleteFile(item.id);
+                                      },
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 34.0),
+                                        decoration: BoxDecoration(
+                                          color: Theme.of(context).errorColor,
+                                        ),
+                                        child: Center(
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              const SizedBox(width: 20.0),
+                                              SvgPicture.asset(
+                                                  'assets/icons/appointments/ic_delete_appointment.svg'),
+                                              const SizedBox(width: 20.0),
+                                            ],
+                                          ),
+                                        ),
                                       ),
                                     ),
                                   ),
@@ -96,20 +100,10 @@ class FilesList extends StatelessWidget {
                                 },
                                 isDownloading: item.id == downloadingFileId,
                               ),
-                            ),
-                          ],
-                        ),
-                        child: FileItem(
-                          fileItem: item,
-                          onTap: () {
-                            handleTapOnUserFile(item);
-                          },
-                          isDownloading: item.id == downloadingFileId,
-                        ),
-                      ))
-                  .toList()
-            ],
-          ),
-        ) : const NotFoundUserFiles());
+                            ))
+                        .toList()
+                  ],
+                ),
+              ));
   }
 }
