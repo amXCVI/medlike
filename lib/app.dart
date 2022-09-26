d920b04735068c2056c22918dc7065cce49b99c6
amXCVI <am.xcvi@gmail.com>
2022-09-26 09:02:05 +0300

    fix build web app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -52,7 +53,7 @@ class App extends StatelessWidget {
           theme: AppTheme.lightAppTheme,
           routerDelegate: AutoRouterDelegate(
             _router,
-            navigatorObservers: () => [
+            navigatorObservers: () => kIsWeb ? [] : [
               FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
             ],
           ),
