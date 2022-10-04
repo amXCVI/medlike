943351e024b7395c7c7df9fac6e9f661e34c82a1
Василий Смоляр <smolyar4@gmail.com>
2022-10-04 22:21:32 +0300

    Fix: autovalidate on focusing

--- lib/modules/health/diary_add_page/form_field.dart
+++ lib/modules/health/diary_add_page/form_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:medlike/themes/colors.dart';
 
-class FormField extends StatelessWidget {
+class FormField extends StatefulWidget {
   const FormField({
     Key? key,
     required this.labelText,
@@ -19,59 +19,80 @@ class FormField extends StatelessWidget {
   final String? Function(String?) validator;
 
   @override
+  State<FormField> createState() => _FormFieldState();
+}
+
+class _FormFieldState extends State<FormField> {
+  late FocusNode _node;
+  bool isFocused = false; 
+
+  @override
+  void initState() {
+    super.initState();
+    _node = FocusNode();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(bottom: 35),
-      child: TextFormField(
-        controller: controller,
-        onChanged: (text) => onChange(text),
-        keyboardType: TextInputType.number,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        inputFormatters: [
-          FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
-        ],
-        decoration: InputDecoration(
-          labelText: labelText,
-          labelStyle: const TextStyle(
-            color: AppColors.lightText,
-            fontSize: 17,
-            fontWeight: FontWeight.w300
+      child: Focus(
+        focusNode: _node,
+        onFocusChange: (focus) => setState(() {
+          isFocused = focus;
+        }),
+        child: TextFormField(
+          controller: widget.controller,
+          onChanged: (text) => widget.onChange(text),
+          keyboardType: TextInputType.number,
+          autovalidateMode: !isFocused
+            ? AutovalidateMode.always : AutovalidateMode.disabled,
+          inputFormatters: [
+            FilteringTextInputFormatter.allow(RegExp(r'^\d+\.?\d{0,2}')),
+          ],
+          decoration: InputDecoration(
+            labelText: widget.labelText,
+            labelStyle: const TextStyle(
+              color: AppColors.lightText,
+              fontSize: 17,
+              fontWeight: FontWeight.w300
+            ),
+            errorStyle: const TextStyle(
+              color: AppColors.mainError,
+              fontSize: 14,
+              fontWeight: FontWeight.w400
+            ),
+            contentPadding: const EdgeInsets.only(top: 4),
+            floatingLabelStyle: TextStyle(
+              color: widget.isEmpty ? AppColors.lightText : AppColors.mainText, 
+              fontWeight: FontWeight.w300
+            ),
+            border: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: widget.isEmpty ? AppColors.lightText : AppColors.mainText
+              )
+            ),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.mainText
+              )
+            ),
+            errorBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: AppColors.mainError
+              )
+            ),
           ),
-          errorStyle: const TextStyle(
-            color: AppColors.mainError,
-            fontSize: 14,
-            fontWeight: FontWeight.w400
+          autofocus: false,
+          enableSuggestions: false,
+          autocorrect: false,
+          textInputAction: TextInputAction.done,
+          style: const TextStyle(
+            color: AppColors.mainText,
+            fontSize: 16
           ),
-          contentPadding: const EdgeInsets.only(top: 4),
-          floatingLabelStyle: TextStyle(
-            color: isEmpty ? AppColors.lightText : AppColors.mainText, 
-            fontWeight: FontWeight.w300
-          ),
-          border: UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: isEmpty ? AppColors.lightText : AppColors.mainText
-            )
-          ),
-          focusedBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: AppColors.mainText
-            )
-          ),
-          errorBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: AppColors.mainError
-            )
-          ),
+          validator: widget.validator,
         ),
-        autofocus: false,
-        enableSuggestions: false,
-        autocorrect: false,
-        textInputAction: TextInputAction.done,
-        style: const TextStyle(
-          color: AppColors.mainText,
-          fontSize: 16
-        ),
-        validator: validator,
       ),
     );
   }
