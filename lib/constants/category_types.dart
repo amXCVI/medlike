e00de4b35a348c092377886c4ffb73eee69514f5
amXCVI <am.xcvi@gmail.com>
2022-10-18 22:18:15 +0300

    fix telemed name

--- lib/constants/category_types.dart
+++ lib/constants/category_types.dart
@@ -1,7 +1,7 @@
 class CategoryTypes {
   static List<CategoryType> categoryTypes = [
     CategoryType(-1, 'Unknown', ''),
-    CategoryType(0, 'Telemed', 'Телемедицинский консультации'),
+    CategoryType(0, 'Telemed', 'Телемедицинские консультации'),
     CategoryType(1, 'Consultations', 'Консультации'),
     CategoryType(2, 'Laboratory', 'Лабораторные исследования'),
     CategoryType(3, 'MRT', 'МРТ'),
