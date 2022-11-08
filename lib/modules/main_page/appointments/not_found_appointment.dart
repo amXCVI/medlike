6400218310e44403cf09b85642a9f74d7a94d69d
Василий Смоляр <smolyar4@gmail.com>
2022-11-08 17:24:05 +0300

    Fix: not found appointment

--- lib/modules/main_page/appointments/not_found_appointment.dart
+++ lib/modules/main_page/appointments/not_found_appointment.dart
@@ -14,55 +14,63 @@ class NotFoundAppointment extends StatelessWidget {
 
     return GestureDetector(
       onTap: _handleTapAppointments,
-      child: Container(
-        margin: const EdgeInsets.all(16.0),
-        padding: const EdgeInsets.all(16.0),
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              AppColors.homeCategoriesGradientColor1,
-              // AppColors.homeCategoriesGradientColor2,
-              AppColors.homeCategoriesGradientColor3,
-            ],
-            stops: [0, 1],
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(12)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 20,
-              offset: Offset(0, 8),
-            ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            Align(
-                alignment: AlignmentDirectional.bottomEnd,
-                child: Image.asset(
-                    'assets/icons/main_page/ic_main_button_three@2x.png',
-                    width: 100)),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'У вас нет предстоящих приемов',
-                  style: Theme.of(context).textTheme.titleMedium,
+      child: Stack(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(16.0),
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  AppColors.homeCategoriesGradientColor1,
+                  // AppColors.homeCategoriesGradientColor2,
+                  AppColors.homeCategoriesGradientColor3,
+                ],
+                stops: [0, 1],
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 20,
+                  offset: Offset(0, 8),
                 ),
-                const SizedBox(height: 6),
-                Text(
-                  'Но это легко исправить, \nзаписавшись к нужному \nспециалисту',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodySmall
-                      ?.copyWith(color: AppColors.lightText),
-                )
               ],
             ),
-          ],
-        ),
+            child: Stack(
+              children: [
+                Align(
+                alignment: AlignmentDirectional.centerEnd,
+                child: Container()),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'У вас нет предстоящих приемов',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    const SizedBox(height: 6),
+                    Text(
+                      'Но это легко исправить, \nзаписавшись к нужному \nспециалисту',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodySmall
+                          ?.copyWith(color: AppColors.lightText),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Image.asset(
+                'assets/icons/main_page/ic_main_stathoscope.png',
+                width: 100))
+        ],
       ),
     );
   }
