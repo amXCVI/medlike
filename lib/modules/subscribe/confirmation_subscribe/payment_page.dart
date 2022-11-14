ec8a6449b53ab0d6a57957ba8a552c60540f1d55
amXCVI <am.xcvi@gmail.com>
2022-11-14 14:07:47 +0300

    fix render payment page in web

--- lib/modules/subscribe/confirmation_subscribe/payment_page.dart
+++ lib/modules/subscribe/confirmation_subscribe/payment_page.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+import 'dart:ui' as ui;
+
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:medlike/domain/app/cubit/subscribe/subscribe_cubit.dart';
@@ -24,32 +28,78 @@ class _PaymentPageState extends State<PaymentPage> {
         return DefaultScaffold(
           appBarTitle: 'Оплата',
           bottomNavigationBar: const SizedBox(),
-          child: WebView(
-            onWebViewCreated: (WebViewController controller) {
-              setState(() {
-                webViewController = controller;
-              });
-            },
-            initialUrl: state.paymentUrl,
-            javascriptMode: JavascriptMode.unrestricted,
-            navigationDelegate: (NavigationRequest request) {
-              if (request.url.contains("result=true")) {
-                context.read<SubscribeCubit>().resetSubscribeStoryState();
-                context.router.push(AppointmentsRoute(isRefresh: true));
-                return NavigationDecision.prevent;
-              }
-              if (request.url.contains("result=false")) {
-                context.router.pop();
-                AppToast.showAppToast(
-                    msg:
-                        "Ошибка оплаты. Попробуйте оплатить прием позднее или наличными");
-                return NavigationDecision.prevent;
-              }
-              return NavigationDecision.navigate;
-            },
-          ),
+          child: kIsWeb
+              ? IframeScreen(
+                  src: state.paymentUrl as String,
+                )
+              : WebView(
+                  onWebViewCreated: (WebViewController controller) {
+                    setState(() {
+                      webViewController = controller;
+                    });
+                  },
+                  initialUrl: state.paymentUrl,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  navigationDelegate: (NavigationRequest request) {
+                    if (request.url.contains("result=true")) {
+                      context.read<SubscribeCubit>().resetSubscribeStoryState();
+                      context.router.push(AppointmentsRoute(isRefresh: true));
+                      return NavigationDecision.prevent;
+                    }
+                    if (request.url.contains("result=false")) {
+                      context.router.pop();
+                      AppToast.showAppToast(
+                          msg:
+                              "Ошибка оплаты. Попробуйте оплатить прием позднее или наличными");
+                      return NavigationDecision.prevent;
+                    }
+                    return NavigationDecision.navigate;
+                  },
+                ),
         );
       },
+    );
+  }
+}
+
+class IframeScreen extends StatefulWidget {
+  const IframeScreen({super.key, required this.src});
+
+  final String src;
+
+  @override
+  _IframeScreenState createState() => _IframeScreenState();
+}
+
+class _IframeScreenState extends State<IframeScreen> {
+  late Widget _iframeWidget;
+
+  final IFrameElement _iframeElement = IFrameElement();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _iframeElement.src = widget.src;
+
+    _iframeElement.style.border = 'none';
+
+    // ignore: undefined_prefixed_name
+    ui.platformViewRegistry.registerViewFactory(
+      'iframeElement',
+      (int viewId) => _iframeElement,
+    );
+
+    _iframeWidget = HtmlElementView(
+      key: UniqueKey(),
+      viewType: 'iframeElement',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: _iframeWidget,
     );
   }
 }
