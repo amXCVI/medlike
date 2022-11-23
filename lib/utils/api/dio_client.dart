03d8054af0b9b8c54f252c8b22e10970aa3c1176
Василий Смоляр <smolyar4@gmail.com>
2022-11-23 14:56:52 +0300

    Feat: ssl pinning

--- lib/utils/api/dio_client.dart
+++ lib/utils/api/dio_client.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:medlike/utils/api/api_constants.dart';
 import 'package:medlike/utils/api/api_interceptors.dart';
+import 'package:http_certificate_pinning/http_certificate_pinning.dart';
 
 class Api {
   static BaseOptions options = BaseOptions(
@@ -12,15 +14,24 @@ class Api {
     receiveTimeout: 30000,
   );
 
-  final Dio _dio = Dio(options);
+  final Dio _dio = Dio()
+    ..options = options
+    ..interceptors.add(DioInterceptors());
+    
 
   Api() {
-    _dio.interceptors.add(DioInterceptors());
+    initSSLCert();
+  }
+
+  void initSSLCert() async {
+    if(!kIsWeb) {
+      _dio.interceptors.add(CertificatePinningInterceptor(
+        allowedSHAFingerprints: ApiConstants.sha256
+      ));
+    }
 
     (_dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
         (HttpClient client) {
-      client.badCertificateCallback =
-          (X509Certificate cert, String host, int port) => true;
       return client;
     };
   }
