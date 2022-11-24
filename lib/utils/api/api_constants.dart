dd3d7ce162ee607442ea9dbdd23ee20045fe63eb
Василий Смоляр <smolyar4@gmail.com>
2022-11-24 08:56:24 +0300

    Fix: 5 env sha256

--- lib/utils/api/api_constants.dart
+++ lib/utils/api/api_constants.dart
@@ -11,6 +11,7 @@ class ApiConstants {
   static String env = 'Zapolyarye';
 
   static List<String> sha256 = [
-    '1F:62:98:94:92:E4:EA:81:B1:7D:79:FC:7B:47:DA:AD:E1:CC:45:D0:F9:99:E4:59:B5:2C:F0:E4:87:E8:6D:C2'
+    '1F:62:98:94:92:E4:EA:81:B1:7D:79:FC:7B:47:DA:AD:E1:CC:45:D0:F9:99:E4:59:B5:2C:F0:E4:87:E8:6D:C2',
+    'ED:CC:84:D0:4E:4B:E0:D3:1C:9F:0E:F7:3F:3E:51:52:C1:9E:48:F8:F3:AC:EC:E8:3D:50:7F:EF:20:A7:90:6A'
   ];
 }
