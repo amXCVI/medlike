d41e756b1aef0805c4da1f2bc4662b68097a5f28
Andrey M <am.xcvi@gmail.com>
2022-11-29 12:21:10 +0300

    fix clinic buildings

--- lib/modules/about_clinic/detail_clinic_with_bottom_sheets/clinic_detail_with_bottom_sheets_page.dart
+++ lib/modules/about_clinic/detail_clinic_with_bottom_sheets/clinic_detail_with_bottom_sheets_page.dart
@@ -34,10 +34,12 @@ class _ClinicDetailWithBottomSheetsPageState
   }
 
   void onTapBuilding(BuildingLatLngModel building) {
-    _panelController.open();
-    setState(() {
-      selectedBuilding = building;
-    });
+    if (!_panelController.isPanelOpen) {
+      setState(() {
+        selectedBuilding = building;
+        _panelController.open();
+      });
+    }
   }
 
   /// Ф-я для свайпа строений клиники
