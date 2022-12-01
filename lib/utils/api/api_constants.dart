bbe3f225c47e52a128bcdd84580d29c78381357b
Andrey M <am.xcvi@gmail.com>
2022-12-01 19:51:28 +0300

    change env

--- lib/utils/api/api_constants.dart
+++ lib/utils/api/api_constants.dart
@@ -2,8 +2,10 @@ class ApiConstants {
   static String baseUrlEnv01 = 'https://tgn.medotrade.ru:23340/medlike/env01';
   static String baseUrlEnv04 = 'https://tgn.medotrade.ru:23340/medlike/env04';
   static String baseUrlEnv05 = 'https://tgn.medotrade.ru:23340/medlike/env05';
+  static String baseUrlZapolyaryeProd =
+      'https://mis-api.nornik.ru/medlike/prod';
 
-  static String baseUrl = baseUrlEnv04;
+  static String baseUrl = baseUrlEnv05;
 
   // Версия приложения. Когда-нибудь нужно сделать, чтобы эта штука подтягивалась автоматически
   static String appVersion = '1.1';
