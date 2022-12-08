65f14a068037a76cd781745a114811e82c9dbb89
Andrey M <am.xcvi@gmail.com>
2022-12-08 17:14:45 +0300

    fix smartapp login page

--- lib/modules/login/smartapp_login/smartapp_login_page.dart
+++ lib/modules/login/smartapp_login/smartapp_login_page.dart
@@ -1,12 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:medlike/constants/app_constants.dart';
 import 'package:medlike/domain/app/cubit/user/user_cubit.dart';
 import 'package:medlike/navigation/router.gr.dart';
 import 'package:medlike/themes/colors.dart';
 import 'package:medlike/utils/api/smaptapp_client.dart';
-import 'package:medlike/utils/user_secure_storage/user_secure_storage.dart';
 
 class SmartappLoginPage extends StatelessWidget {
   const SmartappLoginPage({Key? key, this.resolver}) : super(key: key);
@@ -15,7 +13,7 @@ class SmartappLoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void getSmartappToken() {
+    void getSmartappTokenForce() {
       print('start getting smartapp token');
 
       /// Запрашиваем токен в Смартаппе
@@ -44,6 +42,35 @@ class SmartappLoginPage extends StatelessWidget {
       });
     }
 
+    Future<bool> getSmartappToken() async {
+      print('start getting smartapp token');
+
+      /// Запрашиваем токен в Смартаппе
+      return await SmartAppClient.getSmartAppToken().then((data) {
+        /// Если токен пришел, авторизовываемся у себя через него
+        print(
+            'Токен из смартаппа пришел, его удалось распарсить, передан во внутреннюю авторизацию: $data');
+        return context
+            .read<UserCubit>()
+            .smartappAuth(smartappToken: data)
+            .then((value) {
+          if (value) {
+            /// Если авторизация успешна, переходим на главную
+            print(
+                '!!!!!!!!!! SUCCESS AUTH WITH SMARTAPP TOKEN !!!!!!!!!!!!!!!');
+            return true;
+          } else {
+            print(
+                'Не удалось авторизоваться через smartapp token. Проверьте пользователя');
+            return false;
+          }
+        });
+      }).catchError((onError) {
+        print('ERROR: $onError');
+        return false;
+      });
+    }
+
     void goToLoginPage() {
       context.router.replaceAll([StartPhoneNumberRoute()]);
     }
@@ -52,21 +79,15 @@ class SmartappLoginPage extends StatelessWidget {
       context.router.replaceAll([const MainRoute()]);
     }
 
-    Future<bool> getTestToken() async {
-      //! Запросить токен из смартаппа
-      return Future.delayed(const Duration(seconds: 2), () {
-        return true;
-      });
-    }
-
-    getTestToken().then((bool value) {
+    getSmartappToken().then((bool value) {
       if (value) {
-        //! Записать все токены в localStorage
-        // UserSecureStorage.setField(AppConstants.accessToken, 'value');
-        // UserSecureStorage.setField(AppConstants.refreshToken, 'value');
-        UserSecureStorage.setField(AppConstants.smartappToken, 'value');
-        UserSecureStorage.setField(AppConstants.isAuth, 'true');
-        resolver!.next(true);
+        if (resolver != null) {
+          resolver!.next(true);
+          return;
+        }
+        Future.delayed(const Duration(seconds: 1), () {
+          context.router.replaceAll([const MainRoute()]);
+        });
         return;
       }
     }).catchError((onError) {
@@ -80,7 +101,7 @@ class SmartappLoginPage extends StatelessWidget {
       children: [
         MaterialButton(
           color: AppColors.mainSeparatorAlpha,
-          onPressed: getSmartappToken,
+          onPressed: getSmartappTokenForce,
           child: const Text('auth with smartapp token (sendBotEvent)'),
         ),
         const SizedBox(height: 24),
